235ae8a5e3b3f325b7917f4a7deb5fb8b520a174
Juan Sebastian Herrera Gomez <Jsebastianhg1006@gmail.com>
2021-08-08 00:54:22 -0500

    Actualizacion de reconexion base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,28 @@ const connection = mysql.createConnection({
 	password: '3e9ffce6',
 });
 
+function handleDisconnect(connection) {
+	newConnection = mysql.createPool(connection);
+
+	newConnection.getConnection((err) => {
+		if (err) {
+			console.log('Error de conexion con db:', err);
+			setTimeout(handleDisconnect, 2000);
+		}
+	});
+
+	newConnection.on('error', (err) => {
+		console.log('Db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+handleDisconnect(connection);
+
 // Inicializacion de la conexion con la base de datos
 connection.connect((err) => {
 	if (err) {
