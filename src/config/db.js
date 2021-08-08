aa1e427b861e64f6ad2c7e637614cfcf47f48649
Juan Sebastian Herrera Gomez <Jsebastianhg1006@gmail.com>
2021-08-08 01:05:40 -0500

    Connection error

--- src/config/db.js
+++ src/config/db.js
@@ -1,43 +1,38 @@
 const mysql = require('mysql');
 
 // Establecimiento de los atributos de la conexion mysql
-const connection = mysql.createConnection({
+const DataConnection = {
 	host: 'us-cdbr-east-04.cleardb.com',
 	user: 'b717daa3ce9f57',
 	database: 'heroku_c90549540244ce3',
 	password: '3e9ffce6',
-});
+};
 
-function handleDisconnect(connection) {
-	newConnection = mysql.createPool(connection);
+function handleDisconnect(DataConnection) {
+	const connection = mysql.createPool(DataConnection);
 
-	newConnection.getConnection((err) => {
+	connection.getConnection((err) => {
 		if (err) {
 			console.log('Error de conexion con db:', err);
 			setTimeout(handleDisconnect, 2000);
+		} else {
+			console.log('Conectado exitosamente con la DB');
 		}
 	});
 
-	newConnection.on('error', (err) => {
+	connection.on('error', (err) => {
 		console.log('Db error', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+		if (err.code === 'PROTOCOL_DataConnection_LOST') {
 			handleDisconnect();
 		} else {
 			throw err;
 		}
 	});
+
+	return connection;
 }
 
-handleDisconnect(connection);
-
-// Inicializacion de la conexion con la base de datos
-connection.connect((err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log('Conectado exitosamente con la DB');
-});
+const connection = handleDisconnect(DataConnection);
 
 // Exportacion de la conexion
 module.exports = connection;
